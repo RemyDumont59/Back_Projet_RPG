209323a78533e4bf3c0f1c18841c36d6ae35af6f
03Sly03 <slydevwebproject@outlook.fr>
2023-11-22 13:48:58 +0100

    useCors allowOrigin added

--- Back_Projet_RPG/Back_Projet_RPG/Program.cs
+++ Back_Projet_RPG/Back_Projet_RPG/Program.cs
@@ -19,6 +19,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(option =>
+{
+    option.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+});
+
 app.UseAuthorization();
 
 app.MapControllers();
