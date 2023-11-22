d6b5b97d89a4be8d943192648f2b9f520b37cfc1
Remy DUMONT <dumont.remy.59@gmail.com>
2023-11-22 11:04:55 +0100

    Création Data/DbContext

---
+++ Back_Projet_RPG/Back_Projet_RPG/Data/ApplicationDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Back_Projet_RPG.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Back_Projet_RPG.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Enemy> Enemies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            List<Player> players = new List<Player>()
+            {
+                new Player()
+                {
+                    Id = 1,
+                    LifePoint = 10,
+                    Stamina = 10,
+                    Strength = 10,
+                    Agility = 10,
+                    Intellect = 10,
+                    Luck = 10
+                },
+            };
+
+            List<Enemy> enemies = new List<Enemy>()
+            {
+                new Enemy()
+                {
+                    Id = 1,
+                    LifePoint = 10,
+                    Stamina = 10,
+                    Strength = 10,
+                    Agility = 10,
+                    Intellect = 10,
+                    Luck = 10
+                }
+            };
+        }
+
+    }
+}
